1ef54ebe6bb956a9813a2eba268355bdbb2e810d
Carin Camen <carin@carincamen.com>
2014-09-01 14:55:35 -0500

    Initial working files
    Setup for initial working files css, index.html, js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+function adjustWindow(){
 
+    enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
+
+    // Get window size
+    winH = $window.height();
+    winW = $window.width();
+
+    // Keep minimum height 550
+    if(winH <= 550) {
+        winH = 550;
+    }
+
+    // Init Skrollr for 768 and up
+    if( winW >= 768) {
+
+        // Init Skrollr
+        var s = skrollr.init({
+            forceHeight: false
+        });
+
+        // Resize our slides
+        $slide.height(winH);
+
+        s.refresh($('.homeSlide'));
+
+    } else {
+
+        // Init Skrollr
+        var s = skrollr.init();
+        s.destroy();
+    }
+
+    // Check for touch
+    if(Modernizr.touch) {
+
+        // Init Skrollr
+        var s = skrollr.init();
+        s.destroy();
+    }
+
+}
+
+function initAdjustWindow() {
+    return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+}
