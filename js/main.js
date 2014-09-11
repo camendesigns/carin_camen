aee17ce5963597c4fb9b30a40a9a09edcbc953b5
Carin Camen <carin@carincamen.com>
2014-09-11 10:11:59 -0500

    Make live updates
    Final updates to make the site live.

--- js/main.js
+++ js/main.js
@@ -1,53 +1,76 @@
-function adjustWindow(){
+$(document).ready(function() {
 
-    enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
 
-    // Get window size
-    winH = $window.height();
-    winW = $window.width();
+    /* Scroll event handler */
+    $(window).bind('scroll',function(e){
+        parallaxScroll();
+        redrawDotNav();
+    });
 
-    // Keep minimum height 550
-    if(winH <= 550) {
-        winH = 550;
-    }
-
-    // Init Skrollr for 768 and up
-    if( winW >= 768) {
-
-        // Init Skrollr
-        var s = skrollr.init({
-            forceHeight: false
+    /* Next/prev and primary nav btn click handlers */
+    $('a.web').click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        }, 1000, function() {
+            parallaxScroll(); // Callback is required for iOS
         });
+        return false;
+    });
+    $('a.copy').click(function(){
+        $('html, body').animate({
+            scrollTop:$('#copy').offset().top
+        }, 1000, function() {
+            parallaxScroll(); // Callback is required for iOS
+        });
+        return false;
+    });
+    $('a.design').click(function(){
+        $('html, body').animate({
+            scrollTop:$('#design').offset().top
+        }, 1000, function() {
+            parallaxScroll(); // Callback is required for iOS
+        });
+        return false;
+    });
+    $('a.illustration').click(function(){
+        $('html, body').animate({
+            scrollTop:$('#illustration').offset().top
+        }, 1000, function() {
+            parallaxScroll(); // Callback is required for iOS
+        });
+        return false;
+    });
 
-        // Resize our slides
-        $slide.height(winH);
+    $('a.photography').click(function(){
+        $('html, body').animate({
+            scrollTop:$('#photography').offset().top
+        }, 1000, function() {
+            parallaxScroll(); // Callback is required for iOS
+        });
+        return false;
+    });
 
-        s.refresh($('.homeSlide'));
-
-    } else {
-
-        // Init Skrollr
-        var s = skrollr.init();
-        s.destroy();
-    }
-
-    // Check for touch
-    if(Modernizr.touch) {
-
-        // Init Skrollr
-        var s = skrollr.init();
-        s.destroy();
-    }
-
-}
-
-function initAdjustWindow() {
-    return {
-        match : function() {
-            adjustWindow();
+    /* Show/hide dot lav labels on hover */
+    $('nav#primary a').hover(
+        function () {
+            $(this).prev('h1').show();
         },
-        unmatch : function() {
-            adjustWindow();
+        function () {
+            $(this).prev('h1').hide();
         }
-    };
+    );
+
+});
+
+/* Scroll the background layers */
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bcg1').css('top',(0-(scrolled*.25))+'px');
+    $('#parallax-bcg2').css('top',(0-(scrolled*.5))+'px');
+    $('#parallax-bcg3').css('top',(0-(scrolled*.75))+'px');
 }
+
+
+
+
+
